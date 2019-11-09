1318d31f2d40e2331cf79a123ccf9dcca93c02c8
idleherb <idleherb@posteo.org>
2019-11-09 17:34:26 +0100

    Load merch data on app initialization

--- src/bandcamp_merch_availability/frontend/src/App.jsx
+++ src/bandcamp_merch_availability/frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import fetchMerchItems from './service';
 
 function App() {
-  return (
-    <div>Hello, World!</div>
-  );
+  const [initialized, setInitialized] = useState(false);
+  const [merchItems, setMerchItems] = useState([]);
+
+  useEffect(() => {
+    async function initialize() {
+      try {
+        const merchItems = await fetchMerchItems();
+        if (merchItems) {
+          setMerchItems(merchItems);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setInitialized(true);
+      }
+    }
+    initialize();
+  }, []);
+  
+  const loading = <div />;
+  return initialized ? <div>Hello, World!</div> : loading;
 }
 
 export default App;
--- src/bandcamp_merch_availability/frontend/src/App.test.jsx
+++ src/bandcamp_merch_availability/frontend/src/App.test.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { wait, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
+import fetchMerchItems from './service';
+
+jest.mock('./service');
 
 describe('App', () => {
-  it('should display the application', async () => {
+  it('should call service method when application loads', async() => {
+    const merchItems = [
+      {
+        label: 'aurawire',
+        artist: 'Renjā',
+        title: 'Xenon',
+        release_date: '20180727',
+        timestamp: '2019-11-09T06:03:18.962686',
+        url: 'https://aurawire.bandcamp.com/album/xenon',
+      },
+      {
+        label: 'aurawire',
+        artist: '회사AUTO',
+        title: 'Remix Métaphysique',
+        release_date: '20180831',
+        timestamp: '2019-11-09T06:03:22.184133',
+        url: 'https://aurawire.bandcamp.com/album/remix-m-taphysique',
+      },
+    ];
+    fetchMerchItems.mockReturnValue(merchItems);
+
     const { findByText } = render(<App />);
 
-    const actual = await findByText('Hello, World!');
+    const actual = await findByText('Hello, World!')
     expect(actual).not.toBeNull();
+    expect(fetchMerchItems).toHaveBeenCalled();
   });
 });
